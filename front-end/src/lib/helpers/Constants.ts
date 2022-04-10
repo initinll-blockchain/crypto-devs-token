30d130448492fc2f97a38dc9370d547c63cfe1b6
Nitin Londhe <nitin.londhe@genmills.com>
2022-04-10 23:14:43 +0530

    added networks list

---
+++ front-end/src/lib/helpers/Constants.ts
@@ -0,0 +1,4 @@
+export class Constants {
+    static readonly CONTRACT_ADDRESS_LOCALHOST: string = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+    static readonly CONTRACT_ADDRESS_RINKEBY: string = '0xE20C93f308f791806F8A43c216bf4Cb5f19c0FCc';
+}
